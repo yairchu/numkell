824fd7e37171b1c386929f9b059da5e5381eeef3
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2010-01-15 14:52:25 +0200

    Created Funk. An array-like object that supports broadcasting.

---
+++ src/Data/NumKell/Funk.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, ScopedTypeVariables, TypeFamilies, TypeOperators #-}
+
+module Data.NumKell.Funk where
+
+import Control.Applicative (Applicative(..), liftA2)
+import Data.Generics.Aliases (orElse)
+import Data.HList
+
+-- | HLMaybes transforms an HList
+-- (a :*: b :*: ...)
+-- to a
+-- (Maybe a :*: Maybe b :*: ...)
+type family HLMaybes i
+type instance HLMaybes HNil = HNil
+type instance HLMaybes (a :*: as) = Maybe a :*: HLMaybes as
+
+data Funk i e = Funk
+  { fSize :: HLMaybes i
+  , fIndex :: i -> e -- named after []'s genericIndex
+  }
+
+(!) :: Funk i e -> i -> e
+(!) = fIndex
+
+instance Functor (Funk i) where
+  fmap f a = a { fIndex = fmap f (fIndex a) }
+
+class FunkIdx i where
+  -- dummy (i ->)
+  funkMergeSizes :: i -> HLMaybes i -> HLMaybes i -> HLMaybes i
+  -- dummy (i ->)
+  funkBcastSize :: i -> HLMaybes i
+
+instance FunkIdx HNil where
+  funkMergeSizes _ _ _ = HNil
+  funkBcastSize _ = HNil
+
+instance (Eq a, FunkIdx as)
+  => FunkIdx (HCons a as) where
+  funkMergeSizes dummy (HCons x xs) (HCons y ys)
+    | Just False == liftA2 (==) x y = error "Funk shapes mismatch"
+    | otherwise = HCons (orElse x y) (funkMergeSizes (hTail dummy) xs ys)
+  funkBcastSize = HCons Nothing . funkBcastSize . hTail
+
+instance FunkIdx i => Applicative (Funk i) where
+  pure val =
+    Funk
+    { fSize = funkBcastSize
+    , fIndex = const val
+    }
+  x <*> y =
+    Funk
+    { fSize = funkMergeSizes (undefined :: i) (fSize x) (fSize y)
+    , fIndex = f
+    }
+    where
+      f idx = (fIndex x idx) (fIndex y idx)
+
