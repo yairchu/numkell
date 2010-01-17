05ebab8538f517a5414fc73249fe81a7c0de083c
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2010-01-17 18:17:24 +0200

    refactored SumAxes, no need for functions table

--- src/Data/NumKell/SumAxes.hs
+++ src/Data/NumKell/SumAxes.hs
@@ -20,52 +20,35 @@ type instance FFlattenRes (HCons a as) (HCons TFalse bs)
 type instance FFlattenRes (HCons a as) (HCons TTrue bs)
   = FFlattenRes as bs
 
-data FFlattenFuncs i s =
-  FFlattenFuncs
-  { fFlattenSize :: i -> s -> FFlattenRes i s
-  , fFlattenIdxs :: i -> s -> HCatMaybes (FFlattenRes i s) -> [HCatMaybes i]
-  }
-
 class FFlatten i s where
-  fFlattenFuncs :: FFlattenFuncs i s
+  fFlattenSize :: i -> s -> FFlattenRes i s
+  fFlattenIdxs :: i -> s -> HCatMaybes (FFlattenRes i s) -> [HCatMaybes i]
 
 flattenAxes :: FFlatten i s => Funk i e -> s -> Funk (FFlattenRes i s) [e]
 flattenAxes funk mask =
   Funk
-  { fSize = fFlattenSize tbl (fSize funk) mask
-  , fIndex = map (fIndex funk) . fFlattenIdxs tbl (fSize funk) mask
+  { fSize = fFlattenSize (fSize funk) mask
+  , fIndex = map (fIndex funk) . fFlattenIdxs (fSize funk) mask
   }
-  where
-    tbl = fFlattenFuncs
 
 sumAxes :: (FFlatten i s, Num e) => Funk i e -> s -> Funk (FFlattenRes i s) e
 sumAxes funk = fmap sum . flattenAxes funk
 
 instance FFlatten HNil HNil where
-  fFlattenFuncs =
-    FFlattenFuncs const f
-    where
-      f _ _ _ = [HNil]
+  fFlattenSize _ _ = HNil
+  fFlattenIdxs _ _ _ = [HNil]
 
 instance FFlatten as bs
   => FFlatten (HCons (HJust a) as) (HCons TFalse bs) where
-  fFlattenFuncs =
-    FFlattenFuncs sz idx
-    where
-      sz (HCons x xs) (HCons TFalse ys) =
-        HCons x (fFlattenSize tbl xs ys)
-      idx (HCons _ ss) (HCons TFalse xs) (HCons y ys) =
-        map (HCons y) (fFlattenIdxs tbl ss xs ys)
-      tbl = fFlattenFuncs
+  fFlattenSize (HCons x xs) (HCons TFalse ys) =
+    HCons x (fFlattenSize xs ys)
+  fFlattenIdxs (HCons _ ss) (HCons TFalse xs) (HCons y ys) =
+    map (HCons y) (fFlattenIdxs ss xs ys)
 
 instance (FFlatten as bs, Integral a)
   => FFlatten (HCons (HJust a) as) (HCons TTrue bs) where
-  fFlattenFuncs =
-    FFlattenFuncs sz idx
-    where
-      sz (HCons _ xs) (HCons TTrue ys) =
-        fFlattenSize tbl xs ys
-      idx (HCons (HJust s) ss) (HCons TTrue xs) ys =
-        HCons <$> [0 .. s-1] <*> fFlattenIdxs tbl ss xs ys
-      tbl = fFlattenFuncs
+  fFlattenSize (HCons _ xs) (HCons TTrue ys) =
+    fFlattenSize xs ys
+  fFlattenIdxs (HCons (HJust s) ss) (HCons TTrue xs) ys =
+    HCons <$> [0 .. s-1] <*> fFlattenIdxs ss xs ys
 
