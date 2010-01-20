4927f0399d2476bedc3fe76b8ece35467361db35
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2010-01-20 19:09:31 +0200

    readded liftF2

--- src/Data/NumKell/Funk.hs
+++ src/Data/NumKell/Funk.hs
@@ -14,6 +14,7 @@
 module Data.NumKell.Funk
   ( FunkFuncC(..)
   , Funk(..)
+  , liftF2
   ) where
 
 import Data.HList (HCons(..), HNil(..))
@@ -43,4 +44,16 @@ instance FunkFuncC as b
 
 instance Functor (Funk i) where
   fmap f a = a { funkIndex = fmap f (funkIndex a) }
+
+-- Funk is not an Applicative because 2 Funks may differ in size
+liftF2 :: Eq i => (a -> b -> c) -> Funk i a -> Funk i b -> Funk i c
+liftF2 op fa fb
+  | funkSize fa /= funkSize fb = error "liftF2 of Funks with different shapes"
+  | otherwise =
+    Funk
+    { funkSize = funkSize fa
+    , funkIndex = idx
+    }
+  where
+    idx x = op (funkIndex fa x) (funkIndex fb x)
 
